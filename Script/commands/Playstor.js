89f95e3f901813373d4cef6fa70f2e1c983a33de
tauhid537 <ariyankjan7777@gmail.com>
2025-09-16 13:54:03 +0600

    Update Playstor.js

--- Script/commands/Playstor.js
+++ Script/commands/Playstor.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "playstore",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "☠️ TAUHID IS WITH MIMI ☠️",
   description: "",
   commandCategory: "User",
   usages: "",
