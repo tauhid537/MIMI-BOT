8bb28c8bbe96c69a9ae71bb94e6554c2360bb1e1
tauhid537 <ariyankjan7777@gmail.com>
2025-09-16 13:56:57 +0600

    Update Timkiem.js

--- Script/commands/Timkiem.js
+++ Script/commands/Timkiem.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "search",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+    credits: "☠️ TAUHID IS WITH MIMI ☠️",
     description: "Search results on google",
     commandCategory: "info",
     usages: "search [Text]",
